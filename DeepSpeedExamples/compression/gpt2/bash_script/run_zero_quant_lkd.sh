07bd7232861cbf1119a35a4b5461927a62fea4b2
Bunin Kirill <Bunin.ka@phystech.edu>
2025-10-27 00:33:51 +0300

    add: some

--- DeepSpeedExamples/compression/gpt2/bash_script/run_zero_quant_lkd.sh
+++ DeepSpeedExamples/compression/gpt2/bash_script/run_zero_quant_lkd.sh
@@ -4,7 +4,7 @@
 export PYTHONPATH="/home/buka2004/PTQ-LLM-MIPT:$PYTHONPATH"
 
 CONFIG=/home/buka2004/PTQ-LLM-MIPT/DeepSpeedExamples/compression/gpt2/config/ds_config_W8A8_Qgroup64_fp32.json
-SAVE_PATH=/home/buka2004/PTQ-LLM-MIPT/DeepSpeedExamples/compression/gpt2/out/ZeroQuant/W8A8_quantization_lkd
+SAVE_PATH=/home/buka2004/PTQ-LLM-MIPT/DeepSpeedExamples/compression/gpt2/out/ZeroQuant/W8A8_quantization_lkd_use_prev_quant
 mkdir -p ${SAVE_PATH}
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% if users provide *NO* models, use the following script %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -22,23 +22,49 @@ export CUDA_VISIBLE_DEVICES=2
 
 # Disturbed launch
 python -m torch.distributed.launch --nproc_per_node=1 \
-    --master_port 12346 \
+    --master_port 12345 \
     -m DeepSpeedExamples.compression.gpt2.run_clm_lkd \
     --dataset_name wikitext \
     --dataset_config_name ${dataset_config_name} \
     --model_name_or_path ${MODEL} \
     --per_device_train_batch_size 8 \
-    --num_train_epochs 5 \
+    --num_train_epochs 10 \
     --deepspeed_config ${CONFIG} \
     --deepspeed \
     --device 0 \
-    --learning_rate 5e-6 \
+    --seed 42 \
+    --learning_rate 1e-6 \
     --next_reg_lam 0.1 \
     --weight_decay 0.0 \
     --smooth \
+    --use_prev_quant_layer_input \
     --smooth_output_path ${SAVE_PATH}/act_scales/gpt2-large.pt \
     --smooth_dataset_path /home/buka2004/PTQ-LLM-MIPT/smoothquant/datasets/val.jsonl.zst \
     --num_samples 1024 \
     --seq_len 128 \
     --alpha 0.5 \
     --output_dir ${SAVE_PATH} &>> ${SAVE_PATH}/train.log
+
+# Config 2
+# python -m torch.distributed.launch --nproc_per_node=1 \
+#     --master_port 12346 \
+#     -m DeepSpeedExamples.compression.gpt2.run_clm_lkd \
+#     --dataset_name wikitext \
+#     --dataset_config_name ${dataset_config_name} \
+#     --model_name_or_path ${MODEL} \
+#     --per_device_train_batch_size 8 \
+#     --num_train_epochs 10 \
+#     --deepspeed_config ${CONFIG} \
+#     --deepspeed \
+#     --device 0 \
+#     --seed 42 \
+#     --learning_rate 1e-6 \
+#     --next_reg_lam 0.1 \
+#     --weight_decay 0.0 \
+#     --smooth \
+#     --smooth_output_path ${SAVE_PATH}/act_scales/gpt2-large.pt \
+#     --smooth_dataset_path /home/buka2004/PTQ-LLM-MIPT/smoothquant/datasets/val.jsonl.zst \
+#     --num_samples 1024 \
+#     --seq_len 128 \
+#     --alpha 0.5 \
+#     --output_dir ${SAVE_PATH} &>> ${SAVE_PATH}/train.log
